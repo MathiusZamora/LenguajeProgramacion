d5eefbe739b86368f1c7fc80f9ee788cfca500b2
MathiusZamora <mathiuszamora2@gmail.com>
2024-02-17 09:08:01 -0600

    Update agrege otro codigo

--- ejemplo3.py
+++ ejemplo3.py
@@ -1,1 +1,9 @@
 #listas
+Lista = [1, 2, 3, "Manzana", 15.6, True]
+print (Lista)
+
+#imprimir el primer elemento de la lista
+print(Lista[0])
+Len = len(Lista)
+print(f"El tamaño de la lista es {Len}")
+print(Lista[5])
