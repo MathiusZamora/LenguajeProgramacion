34683d3ec222cc6e20d0f0a3c37984682939fd4d
MathiusZamora <mathiuszamora2@gmail.com>
2024-02-17 09:30:47 -0600

    Update se agregaron codigos

--- ejemplo3.py
+++ ejemplo3.py
@@ -7,3 +7,26 @@ print(Lista[0])
 Len = len(Lista)
 print(f"El tamaño de la lista es {Len}")
 print(Lista[5])
+
+#imprimir los primeros 3 elementos
+print(Lista[0;3])
+
+#imprimir los elementos del 4 a 7
+print(Lista [3;7])
+
+#agregar un elemento a la lista 
+Lista.append
+
+
+#imprimir los ultimos 3 elementos de la lista
+final = len(Lista)
+inicio = final - 3
+print(Lista[inicio: final +1])
+
+#eliminar un elemento de la lista
+Lista.remove("Manzana")
+
+
+#Ordenar listas
+Lista.sort
+print(Lista)
